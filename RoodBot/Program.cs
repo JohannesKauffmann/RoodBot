1130a10fd0251373f6a5f3c0485240bdc1a72398
JohannesMKauffmann <43143244+JohannesMKauffmann@users.noreply.github.com>
2019-01-13 22:26:58 +0100

    initial bot

---
+++ RoodBot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace RoodBot
+{
+    class Program
+    {
+        static DiscordClient discord;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "NTM0MDYwOTU2MjM4NzQxNTE1.Dx0TTQ.UkR9hbwTElGiib8DDnLNYqEPUHs",
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("jij wat maat"))
+                    await e.Message.RespondAsync("G E K O L O N I S E E R D");
+            };
+
+            await discord.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+    }
+}

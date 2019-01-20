85e3ec667d4a2cf6148e8ed63986c9114a9fdf07
JohannesMKauffmann <43143244+JohannesMKauffmann@users.noreply.github.com>
2019-01-20 22:57:07 +0100

    get online status

--- RoodBot/Program.cs
+++ RoodBot/Program.cs
@@ -2,36 +2,42 @@
 using System.Threading.Tasks;
 using DSharpPlus;
 
-namespace RoodBot
-{
-    class Program
-    {
-        static DiscordClient discord;
-
-        static void Main(string[] args)
-        {
-            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
-        }
-
-        static async Task MainAsync(string[] args)
-        {
-            discord = new DiscordClient(new DiscordConfiguration
-            {
-                Token = "NTM0MDYwOTU2MjM4NzQxNTE1.Dx0TTQ.UkR9hbwTElGiib8DDnLNYqEPUHs",
-                TokenType = TokenType.Bot,
-                UseInternalLogHandler = true,
-                LogLevel = LogLevel.Debug
-            });
-
-            discord.MessageCreated += async e =>
-            {
-                if (e.Message.Content.ToLower().StartsWith("ping"))
-                    await e.Message.RespondAsync(e.Message.Timestamp.ToString());
-            };
-
-            await discord.ConnectAsync();
-
-            await Task.Delay(-1);
-        }
-    }
+namespace RoodBot {
+	class Program {
+		static DiscordClient discord;
+		
+		static void Main(string[] args) {
+			MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+		}
+		
+		static async Task MainAsync(string[] args) {
+			discord = new DiscordClient(new DiscordConfiguration {
+				Token = "NTM0MDYwOTU2MjM4NzQxNTE1.Dx0TTQ.UkR9hbwTElGiib8DDnLNYqEPUHs",
+				TokenType = TokenType.Bot,
+				UseInternalLogHandler = true,
+				LogLevel = LogLevel.Debug
+			});
+			
+			discord.MessageCreated += async e => {
+				if (!e.Message.Author.IsCurrent && !e.Message.Author.IsBot) {
+					string status;
+					if (e.Message.Author.Presence != null) {
+						// niet écht offline
+						status = e.Message.Author.Presence.Status.ToString();
+					} else {
+						// je kan niet géén presence hebben en toch een bericht sturen
+						// presence == null betekent dat je invis was en daarna discord hebt opgestart
+						status = "Offline";
+					}
+					if (status == "Offline" || status == "Inivisible") {
+						//TODO: kick
+						await e.Message.RespondAsync(status);
+					}
+				}
+			};
+			
+			await discord.ConnectAsync();
+			await Task.Delay(-1);
+		}
+	}
 }

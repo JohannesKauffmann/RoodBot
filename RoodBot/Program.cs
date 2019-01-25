51abdd91acf8990f915a9b808ecc3b109aedcde0
JohannesMKauffmann <43143244+JohannesMKauffmann@users.noreply.github.com>
2019-01-25 22:41:50 +0100

    lot of changes
    read token from file
    bot could attemt to kick people
    sends DM instead
    token is regenerated tho so f off

--- RoodBot/Program.cs
+++ RoodBot/Program.cs
@@ -1,23 +1,28 @@
 ﻿using System;
 using System.Threading.Tasks;
 using DSharpPlus;
+using DSharpPlus.Entities;
 
 namespace RoodBot {
+
 	class Program {
+
 		static DiscordClient discord;
-		
+
+		static string token = System.IO.File.ReadAllText("..\\..\\..\\token.txt");
+
 		static void Main(string[] args) {
 			MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
 		}
 		
 		static async Task MainAsync(string[] args) {
 			discord = new DiscordClient(new DiscordConfiguration {
-				Token = "NTM0MDYwOTU2MjM4NzQxNTE1.Dx0TTQ.UkR9hbwTElGiib8DDnLNYqEPUHs",
+				Token = token,
 				TokenType = TokenType.Bot,
 				UseInternalLogHandler = true,
 				LogLevel = LogLevel.Debug
 			});
-			
+
 			discord.MessageCreated += async e => {
 				if (!e.Message.Author.IsCurrent && !e.Message.Author.IsBot) {
 					string status;
@@ -29,13 +34,21 @@ namespace RoodBot {
 						// presence == null betekent dat je invis was en daarna discord hebt opgestart
 						status = "Offline";
 					}
+					DiscordMember member = (DiscordMember) e.Author;
 					if (status == "Offline" || status == "Inivisible") {
-						//TODO: kick
-						await e.Message.RespondAsync(status);
+						try {
+							string message = "Darn it, " + member.DisplayName.ToString() + ", you have to be online in order to send messages!";
+							// await member.RemoveAsync(message);
+							DiscordDmChannel channel = await member.CreateDmChannelAsync();
+							// DiscordGuild guild = e.Guild;
+							await channel.SendMessageAsync(message);
+						} catch (DSharpPlus.Exceptions.UnauthorizedException f) {
+							Console.WriteLine(f.JsonMessage);
+						}
 					}
 				}
 			};
-			
+
 			await discord.ConnectAsync();
 			await Task.Delay(-1);
 		}

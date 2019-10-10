cd6eed241c47a1ab25984bdfede0d5b437983fbc
JohannesKauffmann <43143244+JohannesMKauffmann@users.noreply.github.com>
2019-10-10 17:44:07 +0200

    dm

--- RoodBot/Program.cs
+++ RoodBot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.Entities;
@@ -9,41 +10,60 @@ namespace RoodBot {
 
 		static DiscordClient discord;
 
-		static string token = System.IO.File.ReadAllText("..\\..\\..\\token.txt");
+		static readonly string token = System.IO.File.ReadAllText("..\\..\\..\\token.txt");
 
 		static void Main(string[] args) {
 			MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
 		}
 		
 		static async Task MainAsync(string[] args) {
-			discord = new DiscordClient(new DiscordConfiguration {
-				Token = token,
-				TokenType = TokenType.Bot,
-				UseInternalLogHandler = true,
-				LogLevel = LogLevel.Debug
-			});
+			discord = new DiscordClient(
+				new DiscordConfiguration {
+					Token = token,
+					TokenType = TokenType.Bot,
+					UseInternalLogHandler = true,
+					LogLevel = LogLevel.Debug
+				}
+			);
+
+			discord.ClientErrored += async e => {
+				Console.WriteLine("ClientError");
+				Console.WriteLine(e.Exception.InnerException.Message);
+			};
+
+			// var status = member?.Presence?.Status ?? UserStatus.Offline;
 
 			discord.MessageCreated += async e => {
-				if (!e.Message.Author.IsCurrent && !e.Message.Author.IsBot) {
-					string status;
-					if (e.Message.Author.Presence != null) {
-						// niet écht offline
-						status = e.Message.Author.Presence.Status.ToString();
-					} else {
-						// je kan niet géén presence hebben en toch een bericht sturen
-						// presence == null betekent dat je invis was en daarna discord hebt opgestart
-						status = "Offline";
-					}
-					DiscordMember member = (DiscordMember) e.Author;
-					if (status == "Offline" || status == "Inivisible") {
-						try {
-							string message = "Darn it, " + member.DisplayName.ToString() + ", you have to be online in order to send messages!";
-							// await member.RemoveAsync(message);
-							DiscordDmChannel channel = await member.CreateDmChannelAsync();
-							// DiscordGuild guild = e.Guild;
-							await channel.SendMessageAsync(message);
-						} catch (DSharpPlus.Exceptions.UnauthorizedException f) {
-							Console.WriteLine(f.JsonMessage);
+				DiscordMessage currentMessage = e.Message;
+				if (currentMessage.MessageType.Value != MessageType.GuildMemberJoin) {
+					//DiscordMember member = await e.Guild.GetMemberAsync(e.Author.Id);
+					DiscordMember member = e.Guild.Members.First(x => x.Id == e.Author.Id);
+					if (!member.IsCurrent && !member.IsBot) {
+						string status;
+						if (member.Presence != null) {
+							// niet écht offline
+							status = member.Presence.Status.ToString();
+						} else {
+							// je kan niet géén presence hebben en toch een bericht sturen
+							// presence == null betekent dat je invis was en daarna discord hebt opgestart
+							status = "Offline";
+						}
+						if (status == "Offline" || status == "Inivisible") {
+							string discordTag = member.Username + "#" + member.Discriminator;
+							if (member.IsOwner) {
+								await currentMessage.RespondAsync("I can't kick you, but you should be for using invisible mode. Fuck you.");
+								Console.WriteLine("Unsuccesfully tried to kick " + discordTag);
+							} else {
+								string reason = "Let " + discordTag + " rejoin so they can redeem themselves";
+								DiscordInvite invite = await currentMessage.Channel.CreateInviteAsync(86400, 1, false, true, reason);
+								await member.SendMessageAsync(
+									"You have been yeeted from " + e.Guild.Name
+									+ " for using invisible mode. You get a chance to redeem "
+									+ "yourself by using this invite: " + invite.ToString());
+								await e.Guild.RemoveMemberAsync(member, reason);
+								// await member.RemoveAsync(discordTag + " used invisible mode");
+								Console.WriteLine("Succesfully kicked " + discordTag);
+							}
 						}
 					}
 				}
